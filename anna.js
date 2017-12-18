dc620cb6ac30d6fbc45994d798aa6c6e479bf4ca
keiranking <me@keiranking.com>
2017-12-18 02:52:02 -0800

    Creating classes.

--- anna.js
+++ anna.js
@@ -3,7 +3,7 @@
 // ├──────────────────────────────────────────────────────────────────────────┤
 // │ Copyright © 2017 Keiran King (http://keiranking.com)                     │
 // │ Licensed under the Apache License, Version 2.0.                          │
-// │ (https://github.com/keiranking/Eve/blob/master/LICENSE.txt)              │
+// │ (https://github.com/keiranking/Anna/blob/master/LICENSE.txt)              │
 // └──────────────────────────────────────────────────────────────────────────┘
 
 // GLOBAL CONSTANTS ├──────────────────────────────────────────────────────────
@@ -14,8 +14,8 @@ let scoreTable = document.getElementById("score-table");
 let note = null;
 
 // DATA TYPE FUNCTIONS --------------------------------------------------------
-Number.prototype.random = function() { // return random number between 0 and the number (exclusive)
-  return Math.floor(Math.random() * this);
+Number.prototype.random = function(n, p = 0) { // return random number in range
+  return n > p ? Math.floor(Math.random() * (n - p)) + p : Math.floor(Math.random() * (p - n)) + n;
 }
 
 Array.prototype.pluck = function(n = 1) { // return random item, which is deleted from array
@@ -32,6 +32,18 @@ Array.prototype.random = function() { // return random item from array
   return this[this.length.random()];
 }
 
+Array.prototype.sum = function() { // return sum of array elements
+  let sum = 0;
+  for (let i = 0; i < this.length; i++) {
+    sum += this[i];
+  }
+  return sum;
+}
+
+Object.prototype.list = function() {
+  return Object.keys(this);
+}
+
 Object.prototype.randomKey = function() { // return random key from dictionary
   return Object.keys(this).random();
 }
@@ -46,96 +58,44 @@ Object.prototype.flatten = function() { // return flattened array of all nested 
 }
 
 // CLASSES --------------------------------------------------------------------
-class Card {
-  constructor() {
-    this.letter = ALPHABET.random();
-    this.list = this.select();
-    console.log("New card.");
-    this.publish();
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.scores = [];
+    console.log("New player: " + this.name + ".");
   }
 
-  select(n = NO_OF_CATEGORIES) { // populate categories
-    let cats = CATEGORIES.slice(0);
-    switch (localization) {
-      case "all":
-        cats = cats.concat(LOCAL_CATEGORIES.flatten());
-        break;
-      case "none":
-        break;
-      default:
-        cats = cats.concat(LOCAL_CATEGORIES[localization]);
-        break;
-    }
-    return cats.pluck(n);
+  publish() { // send Player contents to UI
+    //
   }
+}
 
-  publish() { // send Card contents to UI
-    categories.innerHTML = "";
-    let ol = document.createElement("OL");
-    for (let i = 0; i < this.list.length; i++) {
-      let li = document.createElement("LI");
-      li.innerHTML = this.list[i];
-      if (i == this.list.length - 1) {
-        li.classList.add("last"); // remove border from last category
+class Game {
+  constructor(names) {
+    console.log("New game.")
+    this.players = {};
+    this.leaderboard = [];
+    if (names) {
+      for (const name of names) {
+        this.seat(name);
       }
-      ol.appendChild(li);
     }
-    categories.appendChild(ol);
+  }
 
-    card.classList.toggle("tilt-left");
-    card.classList.toggle("tilt-right");
+  seat(name) {
+    this.players[name] = [];
+    console.log(name + " added to game.")
+  }
 
-    letter.innerHTML = this.letter;
+  sorted() {
+    return Object.keys(this.players).sort(function(a, b) {
+      return a.sum() - b.sum();
+    }.bind(this));
   }
 }
 
-class Timer {
-  constructor(seconds = ROUND_DURATION) {
-    this.secs = seconds;
-    this.id = null;
-    this.publish();
-  }
-
-  toggle() {
-    clearInterval(this.id);
-    if (this.id) {
-      console.log("Timer paused.");
-      this.id = null;
-      return;
-    }
-    console.log("Timer on.");
-    this.id = setInterval(this.tick.bind(this), 1000); // setInterval inside a class needs .bind(this) to work
-  }
-
-  reset() {
-    clearInterval(this.id);
-    this.id = null;
-    this.secs = ROUND_DURATION;
-    this.publish();
-    console.log("Timer reset.");
-  }
-
-  tick() {
-    this.secs--;
-    if (this.secs < 0) {
-      this.reset();
-      return;
-    }
-    if (this.secs == 0) {
-      audio.play();
-    }
-    this.publish();
-  }
-
-  publish() {
-    if (this.secs <= WARNING_TIME) {
-      timer.classList.add("warning");
-    } else {
-      timer.classList.remove("warning");
-    }
-    timer.innerHTML = this.secs.toTimeString();
-  }
-}
+let g = new Game(["Keiran", "Rae", "Joana", "Monique"]);
+console.log(g.players.list());
 
 class Notification {
   constructor(message, lifetime = undefined) {
@@ -167,43 +127,7 @@ class Notification {
 }
 
 // FUNCTIONS ------------------------------------------------------------------
-function startNewRound() {
-  new Card();
-  t.reset();
-  t.toggle();
-}
 
-function toggleTimer() {
-  t.toggle();
-}
-
-function resetTimer() {
-  t.reset();
-}
-
-function initializeLocalizations() {
-  let l = document.getElementById("localization");
-  let keys = Object.keys(LOCAL_CATEGORIES);
-  keys.push("World");
-  for (let i = 0; i < keys.length; i++) {
-    let option = document.createElement("OPTION");
-    option.value = keys[i];
-    option.innerHTML = keys[i];
-    l.appendChild(option);
-  }
-  l.value = "World";
-}
-
-function report() {
-  let generic = CATEGORIES.length;
-  let local = LOCAL_CATEGORIES.flatten().length;
-  console.log("Categories: " + generic + " generic, " + local + " local, " + (generic + local) + " total.");
-}
-
-function setLocalization() {
-  localization = document.getElementById("localization").value;
-  console.log("Localization: " + localization + ".");
-}
 
 function show(content) {
   if (note) {
@@ -213,8 +137,4 @@ function show(content) {
 }
 
 // MAIN -----------------------------------------------------------------------
-report();
-initializeLocalizations();
-setLocalization();
-let t = new Timer();
-new Card();
+// report();
